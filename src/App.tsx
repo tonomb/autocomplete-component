b9f35f4d9467b5bb10aabd54f9624b064c56b998
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-04-25 14:34:17 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import Autocomplete from "./components/Autocomplete";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Deel's Component Library</h1>
+      <div>
+        <Autocomplete />
+      </div>
     </div>
   );
 }
---
+++ src/components/Autocomplete.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Autocomplete() {
+  return (
+    <div>
+      <h1>Autocomplete Component</h1>
+    </div>
+  );
+}
